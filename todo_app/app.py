e6595a6582a391553617eae99a874bc3f27f4af9
Ashley Bamber <bambera74@gmail.com>
2023-08-31 12:15:49 +0100

    Almost working code but returns none for title

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,6 +4,10 @@ from todo_app.flask_config import Config
 
 from todo_app.data.session_items import get_items
 
+from todo_app.data.session_items import add_item
+
+from flask import request
+
 app = Flask(__name__)
 app.config.from_object(Config())
 
@@ -14,5 +18,12 @@ def index():
     titles = [d['title'] for d in todolist]
     title_one = titles[0]
     title_two = titles[1]
-    return render_template ('index.html', title_one = title_one, title_two = title_two)
+    title_three = titles[2]
+    return render_template ('index.html', title_one = title_one, title_two = title_two, title_three = title_three)
+# Can turn into a for loop if I have time
 
+@app.route('/additem', methods=['GET', 'POST'])
+def additem():
+    title = request.form.get('title')
+    add_item(title)
+    return index()
