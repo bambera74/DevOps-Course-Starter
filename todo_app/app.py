2bb5f5b2e24a697a0acca2c12c33a09277b1ab2a
Ashley Bamber <bambera74@gmail.com>
2023-09-06 09:51:52 +0100

    Revert "Merge pull request #1 from bambera74/exercise-1"
    This reverts commit aab5c2bf50182d6939a608b0f2d5d4415f3f825a, reversing
    changes made to 0e19dee0ff819a726c58509e53da88d82512c838.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 
 from todo_app.flask_config import Config
-
-from todo_app.data.session_items import get_items
-
-from todo_app.data.session_items import add_item
-
-from flask import request, redirect, session
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -14,14 +8,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    todolist = get_items()
-    titles = [d['title'] for d in todolist]
-    return render_template ('index.html', list1=titles)
-
-@app.route('/additem', methods=['GET', 'POST'])
-def additem():
-    if request.method =='POST':
-        title = request.form.get('title1')
-        add_item(title)
-        return redirect (('/'))
-    
+    return 'Hello World!'
