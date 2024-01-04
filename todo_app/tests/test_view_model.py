c5f1be6bfc36967244ecf7465613e9226366b1f7
Ashley Bamber <bambera74@gmail.com>
2024-01-04 13:13:25 +0000

    Attempts to get Item.status working

--- todo_app/tests/test_view_model.py
+++ todo_app/tests/test_view_model.py
@@ -1,60 +1,60 @@
 from operator import length_hint
 from todo_app.view_model import ViewModel
+from todo_app.data.trello_items import Item
 import pytest
 
 @pytest.fixture
 def test_cards():
 
     cards = [
-            Item('1', 'todo', 'testcard1'),
-            Item('2', 'todo', 'testcard2'),
-            Item('3', 'doing', 'testcard3'),
-            Item('4', 'doing', 'testcard4'),
-            Item('5', 'doing', 'testcard5'),
-            Item('6', 'done', 'testcard6'),
+            Item('1', 'testcard1', 'todo'),
+            Item('2', 'testcard2', 'todo'),
+            Item('3', 'testcard3', 'doing'),
+            Item('4', 'testcard4', 'doing'),
+            Item('5', 'testcard5', 'doing'),
+            Item('6', 'testcard6', 'done'),
     ]
     return cards
 
 def test_view_model_handles_items_correctly(test_cards):
     #arrange
-    cards = test_cards
+    items_in_view_model = ViewModel(test_cards)
 
     #act
-    items_in_view_model = ViewModel(cards)
+    items = items_in_view_model.items
 
     #assert
-    assert len(cards) == 6
-    assert items_in_view_model.items == cards
+    assert len(items) == 6
 
 def test_view_model_handles_done_items_correctly(test_cards):
     #arrange
-    cards = [card for card in test_cards if card['status']=="done"] 
-    
+    items_in_view_model = ViewModel(test_cards)
+
     #act
-    items_in_view_model = ViewModel(cards)
+    done_items = items_in_view_model.done_items
 
     #assert
-    assert len(cards) == 1
-    assert items_in_view_model.done_items == cards
+    assert len(done_items) == 1
+    #assert done_items[0].status == "done"
 
 def test_view_model_handles_todo_items_correctly(test_cards):
     #arrange
-    cards = [card for card in test_cards if card['status']=="doing"] 
-    
+    items_in_view_model = ViewModel(test_cards)
+
     #act
-    items_in_view_model = ViewModel(cards)
+    todo_items = items_in_view_model.todo_items
 
     #assert
-    assert len(cards) == 2
-    assert items_in_view_model.doing_items == cards
+    assert len(todo_items) == 2
+    #assert todo_items[0].status == "todo"
 
-def test_view_model_handles_todo_items_correctly(test_cards):
+def test_view_model_handles_doing_items_correctly(test_cards):
     #arrange
-    cards = [card for card in test_cards if card['status']=="doing"] 
-    
+    items_in_view_model = ViewModel(test_cards)
+
     #act
-    items_in_view_model = ViewModel(cards)
+    doing_items = items_in_view_model.done_items
 
     #assert
-    assert len(cards) == 3
-    assert items_in_view_model.doing_items == cards
+    assert len(doing_items) == 3
+    #assert doing_items[0].status == "doing"
